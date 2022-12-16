466e5d66b9fbe81841fbf62f1014d50e168053e7
John Crespi Oncher <johnoncher@gmail.com>
2022-12-16 14:37:15 -0500

    corrected Get and Post for api

--- server/index.js
+++ server/index.js
@@ -9,70 +9,52 @@ const app = express();
 app.use(express.urlencoded({ extended: false}))
 const sqlite3 = require('sqlite3').verbose();
 
-// function createDatabase() {
-//   const newdb = new sqlite3.Database('recipes.db', (err) => {
-//       if (err) {
-//           console.log("Getting error " + err);
-//           exit(1);
-//       }
-//       createTables(newdb);
-//   });
-// }
+function createDatabase() {
+  const newdb = new sqlite3.Database('./db/recipes.db', (err) => {
+      if (err) {
+          console.log("Getting error " + err);
+          exit(1);
+      }
+      createTables(newdb);
+  });
+}
 
-// function createTables(newdb) {
-//   newdb.exec(`
-//   create table recipe (
-//       recipe_id int primary key not null,
-//       title text not null,
-//       image text not null,
-//       discription text not null
-//   );
-//   insert into hero (hero_id, hero_name, is_xman, was_snapped)
-//       values (1, 'Spiderman', 'N', 'Y'),
-//              (2, 'Tony Stark', 'N', 'N'),
-//              (3, 'Jean Grey', 'Y', 'N');
+function createTables(newdb) {
+  newdb.exec(`
+  CREATE TABLE recipes(
+          recipe_id int primary key,
+          title text not null,
+          description  text not null,
+          ingredients text not null,
+          instructions text not null
+      );`
+  );
+}
 
-//   create table hero_power (
-//       hero_id int not null,
-//       hero_power text not null
-//   );
 
-//   insert into hero_power (hero_id, hero_power)
-//       values (1, 'Web Slinging'),
-//              (1, 'Super Strength'),
-//              (1, 'Total Nerd'),
-//              (2, 'Total Nerd'),
-//              (3, 'Telepathic Manipulation'),
-//              (3, 'Astral Projection');
-//       `, ()  => {
-//           runQueries(newdb);
-//   });
-// }
 
 const db = new sqlite3.Database('./db/recipes.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    return console.error(err.message);
-  }
-  console.log('Connected to the in-memory SQlite database.');
+    if (err && err.code == "SQLITE_CANTOPEN") {
+      createDatabase();
+      return;
+      } else if (err) {
+          console.log("Getting error " + err);
+          exit(1);
+    }
+    console.log('Connected to the in-memory SQlite database.');
 });
 
-// db.run(`
-//   CREATE TABLE recipes(
-//           recipe_id int primary key not null,
-//           title,
-//           image,
-//           discription
-//       );`
-// )
 
-const addSQL = `INSERT INTO recipes (
-            recipe_id,
-            title,
-            image,
-            discription)
-            VALUES(?,?,?,?)
 
-  `
+// db.close((err) => {
+//     if (err) {
+//       return console.error(err.message);
+//     }
+//     console.log('Close the database connection.');
+// });
+
+
+
 
   // db.run(sql, [2, "my name jeff", "dsdsdsds", "this is tastey"], (err) => {
   //   if (err) {
@@ -83,7 +65,7 @@ const addSQL = `INSERT INTO recipes (
 
 const data = []
 
-db.all(`SELECT * FROM recipes`, [], (err,rows) => {
+const getCurrentData = () => db.all(`SELECT * FROM recipes`, [], (err,rows) => {
   if (err) {
     return console.error(err.message);
   }
@@ -92,32 +74,39 @@ db.all(`SELECT * FROM recipes`, [], (err,rows) => {
   });
 });
 
+getCurrentData()
+
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
+  getCurrentData()
   res.json({data});
 });
 
-app.post('/api', (req, res) => {
-  let newIndex = data.length + 1
+const addSQL = `INSERT INTO recipes (
+  recipe_id,
+  title,
+  description,
+  ingredients,
+  instructions
+  )
+  VALUES(?,?,?,?,?)
 
-   db.run(addSQL, [newIndex, req.body.title, req.body.image, req.body.discription], (err) => {
+`
+
+app.post('/api', (req, res) => {
+  const currentDBLength = data.length
+
+   db.run(addSQL, [currentDBLength + 1,req.body.title, req.body.description, req.body.ingredients, req.body.instructions], (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log(req.body)
+    getCurrentData()
     res.send("Added recipe to list" )
   })
-
-
-    db.close((err) => {
-      if (err) {
-        return console.error(err.message);
-      }
-      console.log('Close the database connection.');
-    });
 })
 
 // All other GET requests not handled before will return our React app
